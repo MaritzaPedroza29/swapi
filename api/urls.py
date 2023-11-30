d5ee9141164c626ad33291ff3514eba26a7989e3
MaritzaPedroza29 <maritzapedrozacharri@gmail.com>
2023-11-30 17:52:29 -0500

    acomode todo para que fuera automatico

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,10 @@ router.register(r'personajes', views.PersonajesViewSet)
 router.register(r'peliculas', views.PeliculasViewSet)
 router.register(r'planetas', views.PlanetasViewSet)
 router.register(r'especies', views.EspeciesViewSet)
+router.register(r'swapi', views.SWAPIViewSet, basename='swapi')
+router.register(r'swapiplenetas', views.SWAPIPlanetasViewSet, basename='swapiplanetas')
+router.register(r'swapipeliculas', views.SWAPIPlanetasViewSet, basename='swapipeliculas')
+router.register(r'swapiespecies', views.SWAPIEspeciesViewSet, basename='swapiespecies')
 
 urlpatterns = [
     path('', include(router.urls))
